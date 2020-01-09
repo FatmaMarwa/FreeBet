2ff3ec2424d8ea9178f7febe1f03e11b64291931
auffret <louisauffret@outlook.fr>
2020-01-09 16:26:04 +0100

    ajout test 2/validationstests

--- FreeBet-BackEnd/src/test/java/sopra/projet/freebet/FormationTestSpring.java
+++ FreeBet-BackEnd/src/test/java/sopra/projet/freebet/FormationTestSpring.java
@@ -7,10 +7,8 @@ import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import sopra.projet.freebet.model.Admin;
 import sopra.projet.freebet.model.Adress;
@@ -35,14 +33,14 @@ import sopra.projet.freebet.repository.IOpponentRepository;
 import sopra.projet.freebet.repository.ISportRepository;
 import sopra.projet.freebet.repository.IStatisticalRepository;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "/application-context.xml")
+//@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class FormationTestSpring {
 	@Autowired
 	private ISportRepository sportRepo;
 
 	@Autowired
-	private IOpponentRepository opponentRepo;
+	private IOpponentRepository OpponentRepo;
 
 	@Autowired
 	private IGameRepository gameRepo;
@@ -107,36 +105,36 @@ public class FormationTestSpring {
 		Opponent Spurs = new Opponent("San Antonio Spurs", ChampionShip.NBA);
 		Opponent Jazz = new Opponent("Utah Jazz", ChampionShip.NBA);
 		Opponent Kings = new Opponent("Sacramento Kings", ChampionShip.NBA);
-		opponentRepo.save(Hawks);
-		opponentRepo.save(Celtics);
-		opponentRepo.save(Bulls);
-		opponentRepo.save(Hornets);
-		opponentRepo.save(Nets);
-		opponentRepo.save(Cavaliers);
-		opponentRepo.save(Heat);
-		opponentRepo.save(Knicks);
-		opponentRepo.save(Pistons);
-		opponentRepo.save(Magic);
-		opponentRepo.save(Sixers);
-		opponentRepo.save(Pacers);
-		opponentRepo.save(Wizards);
-		opponentRepo.save(Raptors);
-		opponentRepo.save(Bucks);
-		opponentRepo.save(Mavs);
-		opponentRepo.save(Nuggets);
-		opponentRepo.save(Warriors);
-		opponentRepo.save(Rocks);
-		opponentRepo.save(Timberwolves);
-		opponentRepo.save(Clippers);
-		opponentRepo.save(Grizz);
-		opponentRepo.save(Thunder);
-		opponentRepo.save(Lakers);
-		opponentRepo.save(Pels);
-		opponentRepo.save(Blazers);
-		opponentRepo.save(Suns);
-		opponentRepo.save(Spurs);
-		opponentRepo.save(Jazz);
-		opponentRepo.save(Kings);
+		OpponentRepo.save(Hawks);
+		OpponentRepo.save(Celtics);
+		OpponentRepo.save(Bulls);
+		OpponentRepo.save(Hornets);
+		OpponentRepo.save(Nets);
+		OpponentRepo.save(Cavaliers);
+		OpponentRepo.save(Heat);
+		OpponentRepo.save(Knicks);
+		OpponentRepo.save(Pistons);
+		OpponentRepo.save(Magic);
+		OpponentRepo.save(Sixers);
+		OpponentRepo.save(Pacers);
+		OpponentRepo.save(Wizards);
+		OpponentRepo.save(Raptors);
+		OpponentRepo.save(Bucks);
+		OpponentRepo.save(Mavs);
+		OpponentRepo.save(Nuggets);
+		OpponentRepo.save(Warriors);
+		OpponentRepo.save(Rocks);
+		OpponentRepo.save(Timberwolves);
+		OpponentRepo.save(Clippers);
+		OpponentRepo.save(Grizz);
+		OpponentRepo.save(Thunder);
+		OpponentRepo.save(Lakers);
+		OpponentRepo.save(Pels);
+		OpponentRepo.save(Blazers);
+		OpponentRepo.save(Suns);
+		OpponentRepo.save(Spurs);
+		OpponentRepo.save(Jazz);
+		OpponentRepo.save(Kings);
 
 		Adress da1 = new Adress("37 rue Corneille", "31100", "Toulouse", "FRANCE");
 		Adress da2 = new Adress("2 rue de la Paix", "75008", "Paris", "FRANCE");
@@ -192,7 +190,120 @@ public class FormationTestSpring {
 	}
 
 	@Test
-	public void testTwo() {
+	public void testTwo() throws ParseException {
+
+		String pattern = "yyyy-MM-dd HH:mm:ss";
+		SimpleDateFormat Dateevent = new SimpleDateFormat(pattern);
+		Date date1 = Dateevent.parse("2019-12-21 20:00:00");
+		Date date2 = Dateevent.parse("2020-01-10 20:00:00");
+		Date ddp1 = Dateevent.parse("2019-12-20 20:00:00");
+		Date ddp2 = Dateevent.parse("2019-12-19 20:00:00");
+
+		String pattern1 = "yyyy-MM-dd";
+		SimpleDateFormat DateBirth = new SimpleDateFormat(pattern1);
+
+		Date ddn1 = DateBirth.parse("1998-01-01");
+		Date ddn2 = DateBirth.parse("1992-08-08");
+
+		Sport foot = new Sport("Football", TypeSport.three);
+		sportRepo.save(foot);
+
+		Opponent psg = new Opponent("Paris Saint-Germain", ChampionShip.Ligue1);
+		Opponent amiens = new Opponent("Amiens SC", ChampionShip.Ligue1);
+		Opponent dijon = new Opponent("Dijon FCO", ChampionShip.Ligue1);
+		Opponent rennes = new Opponent("Stade Rennais", ChampionShip.Ligue1);
+		Opponent bordeau = new Opponent("Girondins de Bordeaux", ChampionShip.Ligue1);
+		Opponent strasbourg = new Opponent("Strasbourg", ChampionShip.Ligue1);
+		Opponent saintetienne = new Opponent("AS Saint-Etienne", ChampionShip.Ligue1);
+		Opponent monaco = new Opponent("Monaco", ChampionShip.Ligue1);
+		Opponent lille = new Opponent("Lille OSC", ChampionShip.Ligue1);
+		Opponent reims = new Opponent("Stade de Reims", ChampionShip.Ligue1);
+		Opponent lyon = new Opponent("Olympique Lyonnais", ChampionShip.Ligue1);
+		Opponent nantes = new Opponent("Nantes", ChampionShip.Ligue1);
+		Opponent angers = new Opponent("SCO Angers", ChampionShip.Ligue1);
+		Opponent nice = new Opponent("OGC Nice", ChampionShip.Ligue1);
+		Opponent toulouse = new Opponent("Toulouse FC", ChampionShip.Ligue1);
+		Opponent montpellier = new Opponent("Montpellier Hérault", ChampionShip.Ligue1);
+		Opponent brest = new Opponent("Stade Brestois", ChampionShip.Ligue1);
+		Opponent marseille = new Opponent("Olympique de Marseille", ChampionShip.Ligue1);
+		Opponent nimes = new Opponent("Nîmes", ChampionShip.Ligue1);
+
+		OpponentRepo.save(psg);
+		OpponentRepo.save(amiens);
+		OpponentRepo.save(dijon);
+		OpponentRepo.save(rennes);
+		OpponentRepo.save(bordeau);
+		OpponentRepo.save(strasbourg);
+		OpponentRepo.save(saintetienne);
+		OpponentRepo.save(monaco);
+		OpponentRepo.save(lille);
+		OpponentRepo.save(reims);
+		OpponentRepo.save(lyon);
+		OpponentRepo.save(nantes);
+		OpponentRepo.save(angers);
+		OpponentRepo.save(nice);
+		OpponentRepo.save(toulouse);
+		OpponentRepo.save(montpellier);
+		OpponentRepo.save(brest);
+		OpponentRepo.save(marseille);
+		OpponentRepo.save(nimes);
+
+		Adress lad1 = new Adress("11 rue des palourdes", "toulouse", "31000", "France");
+		Adress lad2 = new Adress("12 rue des palourdes", "toulouse", "31000", "France");
+		Adress lad3 = new Adress("13 rue des palourdes", "toulouse", "31000", "France");
+
+		Login ll1 = new Login("Cucupidon", "louis@gmail.com", "abcd1234");
+		Login ll2 = new Login("patapouf", "patapouf@gmail.com", "azerty");
+		Login ll3 = new Login("flicflac", "flicflouf@gmail.com", "motdepasse");
+
+		loginRepo.save(ll1);
+		loginRepo.save(ll2);
+		loginRepo.save(ll3);
+
+		Admin la = new Admin("louis", "auffret", lad1, ll1);
+		adminRepo.save(la);
+
+		Bettor l1bet = new Bettor("Eric", "Sultant", ddn1, 20F, Civility.MR, lad2, ll2);
+		Bettor l2bet = new Bettor("luc", "Blanchot", ddn2, 100F, Civility.MR, lad3, ll3);
+
+		bettorRepo.save(l1bet);
+		bettorRepo.save(l2bet);
+
+		Game amienspsg = new Game(date1, 9.1F, 5.2F, 1.2F, true);
+		Game dijonmetz = new Game(date1, 2.1F, 3.2F, 3.4F, true);
+		Game toulousebrest = new Game(date2, 2.2F, 3.4F, 3.2F, false);
+
+		Statistical st1 = new Statistical(Code.result1, 1);
+		Statistical st2 = new Statistical(Code.result1, 4);
+		Statistical st3 = new Statistical(Code.result1, 2);
+		Statistical st4 = new Statistical(Code.result1, 2);
+
+		List<Statistical> lstats1 = new ArrayList<Statistical>();
+		lstats1.add(st1);
+		lstats1.add(st2);
+		List<Statistical> stats2 = new ArrayList<Statistical>();
+		lstats1.add(st3);
+		lstats1.add(st4);
+
+		statisticalRepo.save(st1);
+		statisticalRepo.save(st2);
+		statisticalRepo.save(st3);
+		statisticalRepo.save(st4);
+
+		gameRepo.save(amienspsg);
+		gameRepo.save(dijonmetz);
+		gameRepo.save(toulousebrest);
+
+		List<Game> amienspsg2 = new ArrayList<Game>();
+		amienspsg2.add(amienspsg);
+		List<Game> dijonmetz2 = new ArrayList<Game>();
+		dijonmetz2.add(dijonmetz);
+
+		Bet lb1 = new Bet(15.5F, true, TypeBet.simple, ddp1, 18.6F, amienspsg2);
+		Bet lb2 = new Bet(100F, false, TypeBet.combined, ddp2, 340F, dijonmetz2);
+
+		betRepo.save(lb1);
+		betRepo.save(lb2);
 
 	}
 
