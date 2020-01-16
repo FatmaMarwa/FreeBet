dba0fb7010b702bd4d3cf91f370c160cd2d5001d
Fatma <rachdi.marwa@gmail.com>
2020-01-16 16:19:40 +0100

    game

--- FreeBet-Angular/src/app/app.module.ts
+++ FreeBet-Angular/src/app/app.module.ts
@@ -11,6 +11,20 @@ import { BetComponent } from './bet/bet.component';
 
 import { SportComponent } from './sport/sport.component';
 import {FormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { OpponentComponent } from './opponent/opponent.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { OpponentComponent } from './opponent/opponent.component';
 
 
 @NgModule({
@@ -21,7 +35,9 @@ import {FormsModule} from '@angular/forms';
     SportComponent,
     AppComponent,
     AdminComponent,
-    LoginComponent
+    LoginComponent,
+    GameComponent,
+    OpponentComponent
   ],
   imports: [
     BrowserModule,
