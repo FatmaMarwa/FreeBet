58817ffe6558ef69d023d8c0c375a8ccb5424abd
Fatma <rachdi.marwa@gmail.com>
2020-01-16 16:22:22 +0100

    correction app module

--- FreeBet-Angular/src/app/app.module.ts
+++ FreeBet-Angular/src/app/app.module.ts
@@ -1,28 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
-
 import { BettorComponent } from './bettor/bettor.component';
 import { BetComponent } from './bet/bet.component';
-
 import { SportComponent } from './sport/sport.component';
 import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GameComponent } from './game/game.component';
-import { OpponentComponent } from './opponent/opponent.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { OpponentComponent } from './opponent/opponent.component';
 
