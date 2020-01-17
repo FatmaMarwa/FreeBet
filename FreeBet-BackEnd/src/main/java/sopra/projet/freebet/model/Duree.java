8d91d4a118ad0bbe1070f6e13b1c86ce171e627c
dd <dd>
2020-01-17 16:49:15 +0100

    1649

--- FreeBet-BackEnd/src/main/java/sopra/projet/freebet/model/Duree.java
+++ FreeBet-BackEnd/src/main/java/sopra/projet/freebet/model/Duree.java
@@ -1,16 +1,25 @@
 package sopra.projet.freebet.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@JsonPropertyOrder({"name", "duree"})
 public enum Duree {
-A(130), B(180), C(400);
-	
+	A(130), B(180), C(400);
+
 	private int duree;
 
-    Duree(int duree) {
-        this.duree = duree;
-    }
+	Duree(int duree) {
+		this.duree = duree;
+	}
+	
+	public String getName() {
+		return name();
+	}
 
-    public int getNumVal() {
-        return duree;
-    }
+	public int getDuree() {
+		return duree;
+	}
+
 }
-
