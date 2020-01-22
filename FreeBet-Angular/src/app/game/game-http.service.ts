0ecec0e59400f7f778a19590c9f38c7717d295c5
dmnpjs <dmn.pjs@gmail.com>
2020-01-22 16:57:33 +0100

    maj views

--- FreeBet-Angular/src/app/game/game-http.service.ts
+++ FreeBet-Angular/src/app/game/game-http.service.ts
@@ -17,11 +17,10 @@ import {Sport} from '../Model/sport';
 export class GameHttpService {
   games: Array<Game>;
   sports: Array<Sport>;
-  opponents : Array<Opponent>
+  opponents: Array<Opponent>;
 
 
-
-  constructor(private appConfig: AppConfigService, private http: HttpClient,private opponentService:OpponentHttpService ) {
+  constructor(private appConfig: AppConfigService, private http: HttpClient, private opponentService: OpponentHttpService) {
     this.load();
     this.loadsport();
     this.loadopponent();
@@ -33,26 +32,30 @@ export class GameHttpService {
       },
       err => console.log(err));
   }
-    loadsport() {
-      this.http.get<Array<Sport>>(this.appConfig.backEnd + 'sport').subscribe(resp => {
-          this.sports = resp;
-        },
-        err => console.log(err));
-    }
-      loadopponent() {
-        this.http.get<Array<Opponent>>(this.appConfig.backEnd + 'opponent').subscribe(resp => {
-            this.opponents = resp;
-          },
-          err => console.log(err))
-      }
-        loadchampionnat() {
-          this.http.get<Array<Opponent>>(this.appConfig.backEnd + 'opponent').subscribe(resp => {
-              this.opponents = resp;
-            },
-            err => console.log(err))
+
+  loadsport() {
+    this.http.get<Array<Sport>>(this.appConfig.backEnd + 'sport').subscribe(resp => {
+        this.sports = resp;
+      },
+      err => console.log(err));
+  }
+
+  loadopponent() {
+    this.http.get<Array<Opponent>>(this.appConfig.backEnd + 'opponent').subscribe(resp => {
+        this.opponents = resp;
+      },
+      err => console.log(err));
+  }
+
+  loadchampionnat() {
+    this.http.get<Array<Opponent>>(this.appConfig.backEnd + 'opponent').subscribe(resp => {
+        this.opponents = resp;
+      },
+      err => console.log(err));
 
   }
-  findAll(): Array<Game>{
+
+  findAll(): Array<Game> {
     return this.games;
   }
 
@@ -60,27 +63,31 @@ export class GameHttpService {
     return this.http.get<Array<Game>>(this.appConfig.backEnd + 'game');
   }
 
-  findById(id: number): Observable<Game>{
+  findAllObservableBySport(id: number): Observable<Array<Game>> {
+    return this.http.get<Array<Game>>(this.appConfig.backEnd + 'sport/' + id + '/games');
+  }
+
+  findById(id: number): Observable<Game> {
     return this.http.get<Game>(this.appConfig.backEnd + 'game/' + id);
   }
 
-  save(gamesport: Game){
-    if(gamesport){
-      if(!gamesport.id){
-        this.http.post<Game>(this.appConfig.backEnd + 'game', gamesport).subscribe( resp =>{
+  save(gamesport: Game) {
+    if (gamesport) {
+      if (!gamesport.id) {
+        this.http.post<Game>(this.appConfig.backEnd + 'game', gamesport).subscribe(resp => {
           this.load();
         }, err => console.log(err));
-      }else {
-        this.http.put<Game>(this.appConfig.backEnd + 'game/' + gamesport.id, gamesport).subscribe(resp =>{
+      } else {
+        this.http.put<Game>(this.appConfig.backEnd + 'game/' + gamesport.id, gamesport).subscribe(resp => {
           this.load();
         }, err => console.log(err));
       }
     }
   }
 
-  delete(id: number){
-    this.http.delete<Game>(this.appConfig.backEnd + 'game/' + id).subscribe(resp =>{
-      this.load()
+  delete(id: number) {
+    this.http.delete<Game>(this.appConfig.backEnd + 'game/' + id).subscribe(resp => {
+      this.load();
     }, err => console.log(err));
   }
 
