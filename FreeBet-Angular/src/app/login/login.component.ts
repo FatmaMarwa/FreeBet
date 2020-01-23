3f789736620fdc34b71ca3260ae9400c67806863
dd <dd>
2020-01-23 09:51:49 +0100

    951

--- FreeBet-Angular/src/app/login/login.component.ts
+++ FreeBet-Angular/src/app/login/login.component.ts
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
 
   login: Login
 
-  constructor(private modalService: NgbModal, private loginService: LoginHttpService, private adminService: AdminHttpService, private bettorService: BettorHttpService) {
+  constructor(private modalService: NgbModal, private loginService: LoginHttpService, private adminService: AdminHttpService, private bettorService: BettorHttpService, private homeService: HomeService) {
 
 
   }
